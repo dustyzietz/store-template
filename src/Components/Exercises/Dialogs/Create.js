aaa307fd86b867d009efdf860caecc125d9c662f
Dusty Zietz <dustyzietz@gmail.com>
2019-08-12 17:55:10 -1000

    added dialog

--- src/Components/Exercises/Dialogs/Create.js
+++ src/Components/Exercises/Dialogs/Create.js
@@ -1,6 +1,67 @@
-import React from 'react';
 
-export default props =>
-<div>
-    
-</div>
+import React, { Fragment, Component } from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import CheckCircleOutline from '@material-ui/icons/CheckCircleOutline';
+import AddIcon from '@material-ui/icons/Add';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Fab from '@material-ui/core/Fab';
+
+
+export default class extends Component {
+  state = {
+    open: false,
+    title: '',
+     
+  };
+
+  handleTogle = () => {
+    this.setState({
+      open : !this.state.open 
+    })
+  }
+
+ handleChange = name => event => {
+    this.setState({ [name]: event.target.value });
+  };
+
+render() {
+const {open, title} = this.state ;
+return(
+<Fragment>
+<Fab onClick={this.handleTogle} aria-label="add"  size="small">
+        <AddIcon />
+      </Fab>
+<Dialog onClose={this.handleTogle} open={open} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">
+            Create a new Exercise
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+           Please fill out the form below
+          </DialogContentText>
+        <form>
+        <TextField
+        id="standard-name"
+        label="Title"
+        value={title}
+        onChange={this.handleChange('title')}
+        margin="normal"
+      />
+        </form>
+        </DialogContent>
+        <DialogActions>
+          <Button variant='contained' color="primary">
+            Create
+          </Button>
+        </DialogActions>
+      </Dialog>
+</Fragment>
+)
+}}
--- src/Components/Layouts/Header.js
+++ src/Components/Layouts/Header.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import { Toolbar, Typography, AppBar } from '@material-ui/core';
+
+import CreateDialog from '../Exercises/Dialogs/Create'
 import { makeStyles } from '@material-ui/core/styles';
-// import IconButton from '@material-ui/core/IconButton';
-// import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles(theme => ({
     root: {
       flexGrow: 1,
     },
+    title: {
+      flexGrow: 1,
+    },
   }));
+
    
 export default function Header() { 
-    const classes = useStyles();
+  const classes = useStyles();
     return (
         <AppBar position="static">
         <Toolbar>
-          <Typography variant='h6' color='inherit'>
+          <Typography variant='h6' color='inherit' className={classes.title} >
             Exercise Database
           </Typography>
+          <CreateDialog/>
         </Toolbar>
       </AppBar>
     )
